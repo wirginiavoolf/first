65abee3647480960aa48d8131dee56cc03fe1318
Wirginia Voolf <stephzanie@gmail.com>
2016-08-28 18:09:52 -0400

    Made a new file
    Made a new file

---
+++ sketch.js
@@ -0,0 +1,13 @@
+/*
+Tutorial via http://creative-coding.decontextualize.com/first-steps/
+*/
+
+function setup () {
+	
+}
+
+function draw () {
+	background (240);
+	ellipse (50, 50, 60, 60);
+	rect (40, 40, 35, 45)
+}
