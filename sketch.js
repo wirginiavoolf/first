08a2072ac3985b9cd6e1c9ebb531391e3ceed45b
Wirginia Voolf <stephzanie@gmail.com>
2016-08-28 21:10:43 -0400

    Added new codes

--- sketch.js
+++ sketch.js
@@ -3,11 +3,15 @@ Tutorial via http://creative-coding.decontextualize.com/first-steps/
 */
 
 function setup () {
+	createCanvas (400, 400)
 	
 }
 
 function draw () {
-	background (240);
-	ellipse (50, 50, 60, 60);
-	rect (40, 40, 35, 45)
+	background (100, 149, 237);
+	strokeWeight (20);
+	stroke (252, 108, 133);
+	ellipse (170, 150, 250, 250);
+	stroke (240);
+	rect (150, 95, 200, 250)
 }
