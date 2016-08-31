6d394385722b06d205f665a29f31e4abaedca172
wirginiavoolf <stephzanie@gmail.com>
2016-08-31 19:29:40 -0400

    new file

---
+++ faces.js/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+  
+  //eyes
+  ellipse(50,50,100,100);
+  ellipse(150,50,100,100);
+  
+  //pupils
+  ellipse(50,50,50,50);
+  ellipse(150,50,50,50);
+  
+  //nose
+  rect(125,125,30,30);
+  
+  
+//mouth
+  rect(50,200,200,200, 0,20,50,20);
+  
+  //teeth
+line(50,300, 250,300);
+line(125,200, 125,400);
+line(175,200, 175,400);
+  
+}
