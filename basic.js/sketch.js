b53b4d8ee2b4234ea295ebebe7409078c5984cf0
wirginiavoolf <stephzanie@gmail.com>
2016-08-31 19:20:29 -0400

    worked in a canvas

---
+++ basic.js/sketch.js
@@ -0,0 +1,19 @@
+var num = 100;
+var num1 = 1;
+var word = "hello";
+var word1 = "goodbye";
+
+function setup() {
+  console.log(num)
+  console.log(num + num1)
+  console.log(word + word1)
+  //this will not work bc u cannot subtract 2 words
+  //console.log(word - word1)
+  
+  console.log("num: " + num);
+  console.log(word + " " + word1)
+}
+
+function draw() {
+  //console.log(num);
+}
